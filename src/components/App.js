2b66c198c7c60eb37a478b5ccc0de3de6647baa8
wim <wiliam.tae@gmail.com>
2019-11-18 07:33:03 -0200

    PageNotFound added

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,19 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import HomePage from './home/HomePage'
 import AboutPage from './about/AboutPage'
 import Header from './common/Header'
+import PageNotFound from './PageNotFound'
+
 const App = () => (
  <div className='container-fluid'>
    <Header/>
-   <Route exact path='/' component={HomePage}/>
-   <Route path='/about' component={AboutPage}/>
- </div>
+   <Switch>
+    <Route exact path='/' component={HomePage}/>
+    <Route path='/about' component={AboutPage}/>
+    <Route component={PageNotFound}/>
+ </Switch>
+</div>
 )
 
 export default App
---
+++ src/components/PageNotFound.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const PageNotFound = () => <p>Sorry, this page does not exist, =( </p>
+
+export default PageNotFound
