be2d548937f2b35b256490bced52dbc2d012ae9f
wim <wiliam.tae@gmail.com>
2019-11-20 09:46:05 -0200

    handling loading courses

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,5 +1,20 @@
 'use strict'
 import * as type from './actionsTypes'
+import * as courseApi from '../../api/courseApi'
 
 export const createCourse = (course) => ( { type: type.CREATE_COURSE, course } )
 
+export const leadCourseSuccess = (courses) => ( { type: type.LOAD_COURSES_SUCCESS, courses } )
+
+export function loadCourses() {
+  return function (dispatch) {
+    return courseApi
+    .getCourses()
+    .then(courses => {
+      dispatch(leadCourseSuccess(courses))
+    })
+    .catch(error => {
+      throw error
+    })
+  }
+}
